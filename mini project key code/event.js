b1c98148ab96117a40283138d72d73ff0fac97d8
mhsnh952 <mhsnh952@gmail.com>
2023-06-01 11:10:39 +0330

    update5

--- mini project key code/event.js
+++ mini project key code/event.js
@@ -4,13 +4,15 @@ let resultspan2 = document.querySelector(".result2");
 let child = document.querySelector(".bottem");
 let resultspan3 = document.querySelector(".result3");
 
-child.onkeydown = function () {
+/* child.onkeydown = function () {
   child.style.display = "block";
-};
+}; */
 document.body.onkeydown = function (event) {
   event.preventDefault();
   child.style.display = "block";
   resultspan1.innerHTML = "event.keyCode" + event.keyCode;
   resultspan2.innerHTML = "event.key" + event.key;
   resultspan3.innerHTML = "event.location" + event.location;
+  console.log(child.nodeName);
+  console.log(child.nodeType);
 };
