0604e08705a7a752c82edc5224705a425e346f9a
mhsnh952 <mhsnh952@gmail.com>
2023-06-03 11:01:49 +0330

    update10

---
+++ todolist/source/js/script.js
@@ -0,0 +1,34 @@
+﻿let $ = document;
+let inputElem = $.querySelector("input");
+let addTodoForm = $.querySelector(".add");
+let todoUlElem = $.querySelector(".todos");
+
+function addnewtodo(newtodovalue) {
+  let newtodoli = $.createElement("li");
+  newtodoli.className =
+    "list-group-item d-flex justify-content-between align-items-center";
+  let newtodolistspan = $.createElement("span");
+  newtodolistspan.innerHTML = newtodovalue;
+  let newtodotrash = $.createElement("i");
+  newtodotrash.className = "fa fa-trash-o delete";
+  todoUlElem.append(newtodoli);
+  newtodoli.append(newtodolistspan, newtodotrash);
+  console.log(newtodoli);
+  newtodotrash.addEventListener("click", function (event) {
+    event.target.parentElement.remove();
+  });
+}
+addTodoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+
+inputElem.addEventListener("keydown", function (event) {
+  let newtodovalue = event.target.value.trim();
+  console.log(newtodovalue);
+  if (event.keyCode == 13) {
+    if (newtodovalue) {
+      inputElem.value = "";
+      addnewtodo(newtodovalue);
+    }
+  }
+});
