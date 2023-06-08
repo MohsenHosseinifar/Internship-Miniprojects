f5935491d11be50b268a479bb7408e62ebf6ad52
mhsnh952 <mhsnh952@gmail.com>
2023-06-08 10:21:36 +0330

    update1

---
+++ nav-bar-transition/source/js/app.js
@@ -0,0 +1,14 @@
+let mainNav = document.getElementById("mainNav");
+let logo = document.querySelector("img");
+document.addEventListener("scroll", function () {
+  if (document.documentElement.scrollTop > 0) {
+    mainNav.classList.add("bg-black");
+    mainNav.classList.add("txt-white");
+    logo.style.height = "64px";
+  }
+  if (document.documentElement.scrollTop === 0) {
+    mainNav.classList.remove("bg-black");
+    mainNav.classList.remove("txt-white");
+    logo.style.height = "84px";
+  }
+});
